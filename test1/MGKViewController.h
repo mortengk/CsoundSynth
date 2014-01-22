c6c3cc9f103c51b428c77d565c1f5ad941f87669
Morten Kleveland <mortengkleveland@gmail.com>
2014-01-22 13:33:43 +0100

    Initial Commit

---
+++ test1/MGKViewController.h
@@ -0,0 +1,13 @@
+//
+//  MGKViewController.h
+//  test1
+//
+//  Created by Morten Kleveland on 22.01.14.
+//  Copyright (c) 2014 NTNU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGKViewController : UIViewController
+
+@end
---
+++ test1/MGKViewController.m
@@ -0,0 +1,29 @@
+//
+//  MGKViewController.m
+//  test1
+//
+//  Created by Morten Kleveland on 22.01.14.
+//  Copyright (c) 2014 NTNU. All rights reserved.
+//
+
+#import "MGKViewController.h"
+
+@interface MGKViewController ()
+
+@end
+
+@implementation MGKViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
